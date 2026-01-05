3fa18a4f59e08247c5b5c5b48a69d62ece26ab0e
AndreaLeang <andrealeang@gmail.com>
2026-01-04 19:16:04 -0500

    fixed added store timer

--- flexllmgen/pytorch_backend.py
+++ flexllmgen/pytorch_backend.py
@@ -867,17 +867,17 @@ def general_copy(dst: TorchTensor, dst_indices: Tuple[slice],
             KVLoadTimer.stop()
     else:
         # The normal path
+        started_timer = False
         if (src.device.device_type == DeviceType.CUDA and
-          dst.device.device_type == DeviceType.CPU and
+          dst.device.device_type == DeviceType.CPU and 
           kv_copy == 2 and KVStoreTimer is not None):
+            started_timer = True
             KVStoreTimer.start()
         src = src.data[src_indices] if src_indices else src.data
         dst = dst.data[dst_indices] if dst_indices else dst.data
         dst.copy_(src, non_blocking=True)
         
-        if (src.device.device_type == DeviceType.CUDA and
-          dst.device.device_type == DeviceType.CPU and
-          kv_copy == 2 and KVStoreTimer is not None):
+        if started_timer:
             KVStoreTimer.stop()
 
 
