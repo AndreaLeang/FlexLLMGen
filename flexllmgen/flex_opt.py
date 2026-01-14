40f211504d620f2a8666a89847fc0a5c8d6c2b1a
AndreaLeang <andrealeang@gmail.com>
2026-01-14 10:44:48 -0500

    debug default removed

--- flexllmgen/flex_opt.py
+++ flexllmgen/flex_opt.py
@@ -1319,7 +1319,7 @@ def add_parser_arguments(parser):
     parser.add_argument("--cut-gen-len", type=int,
         help="Cut generation length for fast debugging.")
     parser.add_argument("--debug-mode", type=str,
-        choices=["fewer_batch", "breakdown"], default="None")
+        choices=["fewer_batch", "breakdown"])
     parser.add_argument("--gpu-batch-size", type=int, default=4)
     parser.add_argument("--num-gpu-batches", type=int, default=1)
     parser.add_argument("--percent", nargs="+", type=int,
