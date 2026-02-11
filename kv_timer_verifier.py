3af8a9c2ec4f226044dec0c316eeb0a2ca0b164e
AndreaLeang <andrealeang@gmail.com>
2026-02-11 11:39:40 -0500

    fixed filenames

--- kv_timer_verifier.py
+++ kv_timer_verifier.py
@@ -112,15 +112,19 @@ def get_all_gpu_memcpy_correlations(json_filename, get_cpu_time=False, est_bandw
                     cur_storing_idx += 1
 
     if record_ind_events:
-        cur_filename = json_filename[:-5] + '.csv'
+        cur_filename = json_filename[:-5] 
         print(f"cur_filename: {cur_filename}")
-        with open(cur_filename, 'w', newline='') as csvfile:
+        with open(cur_filename + '_loading.csv', 'w', newline='') as csvfile:
             fieldnames = ['idx', 'data (GB)', 'bandwidth (GB/s)']
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
             writer.writeheader()
 
             for idx in range(cur_loading_idx):
                 writer.writerow({'idx': idx, 'data (GB)': loading_events[idx][0] / 1000000000.0, 'bandwidth (GB/s)': loading_events[idx][1]})
+        with open(cur_filename + '_storing.csv', 'w', newline='') as csvfile:
+            fieldnames = ['idx', 'data (GB)', 'bandwidth (GB/s)']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
             for idx in range(cur_storing_idx):
                 writer.writerow({'idx': idx, 'data (GB)': storing_events[idx][0] / 1000000000.0, 'bandwidth (GB/s)': storing_events[idx][1]})
 
