0face5282e22f90342a21a4a666a7b3db8462a18
Andrea Leang <42904081+AndreaLeang@users.noreply.github.com>
2026-02-22 15:26:46 -0800

    all csv

--- kv_timer_verifier.py
+++ kv_timer_verifier.py
@@ -236,7 +236,7 @@ def get_all_gpu_memcpy_correlations(json_filename, get_cpu_time=False, est_bandw
     #     est_storing_bandwidth = total_storing_bytes / total_storing_cache_time_gpu # GB / s
 
     # Check if file exists to write header only once
-    csv_filename = 'trace_stats_' + json_filename[:-41]  + '.csv'
+    csv_filename = json_filename[:-41]  + '_trace_stats.csv'
     kv_gpu_percent = int(json_filename.split('-')[9])
     fieldnames = ['kv_gpu_percent', 'tot_loading_time_gpu (s)', 'tot_storing_time_gpu (s)', 'tot_loading_time_cpu (s)','tot_storing_time_cpu (s)',  'total_loading_bytes (GB)', 'total_storing_bytes (GB)']
 
