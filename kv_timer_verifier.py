30fc6af14f01e7d842eb26276f532991bd13dddb
Andrea Leang <42904081+AndreaLeang@users.noreply.github.com>
2026-02-22 15:29:02 -0800

    all csv

--- kv_timer_verifier.py
+++ kv_timer_verifier.py
@@ -212,7 +212,7 @@ def get_all_gpu_memcpy_correlations(json_filename, get_cpu_time=False, est_bandw
                     writer.writerow({'idx': idx, 'og Index': one_dir_loading_events[idx][0], 'data (B)': one_dir_loading_events[idx][1] , 'bandwidth (GB/s)': one_dir_loading_events[idx][2]})
             
             # One Direction Store
-            with open(cur_filename + 'one_storing.csv', 'w', newline='') as csvfile:
+            with open(cur_filename + '_one_storing.csv', 'w', newline='') as csvfile:
                 fieldnames = ['idx', 'og Index', 'data (B)', 'bandwidth (GB/s)']
                 writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
                 writer.writeheader()
