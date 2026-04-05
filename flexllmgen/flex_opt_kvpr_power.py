b5b63c3fe33eb4af078550f86c1091ddd9729c05
Andrea Leang <42904081+AndreaLeang@users.noreply.github.com>
2026-04-05 15:22:05 -0700

    more prints

--- flexllmgen/flex_opt_kvpr_power.py
+++ flexllmgen/flex_opt_kvpr_power.py
@@ -374,6 +374,7 @@ class SelfAttention:
             if self.policy.attn_sparsity >= 1.0:
                 print(f"k_home: {k_home}")
                 print(f"dst: {dst}")
+                print(f"indicies: {indices}")
                 cache_read_buf.store_pow((
                     k_home.smart_copy(dst, indices, kv_copy=1, KVLoadTimer=KVLoadTimer),
                     v_home.smart_copy(dst, indices, kv_copy=1, KVLoadTimer=KVLoadTimer),
