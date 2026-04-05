7f4991b32da06176090db5ca65b298ef87c30357
Andrea Leang <42904081+AndreaLeang@users.noreply.github.com>
2026-04-05 14:21:42 -0700

    fixing definition

--- rapl-nvml-power-monitor-main/llm_bench.py
+++ rapl-nvml-power-monitor-main/llm_bench.py
@@ -340,7 +340,6 @@ class LLMPowerBench:
                 for i in range(self.model.execute_gen_len):
                     self.model.update_attention_mask(i, 0)
                     for j in range(num_layers):
-                        all_acc_layers[j+i*num_layers].token_gen = i
                         for each_iter in range(n_iters_layer):
                             # print(f"each_iter: {each_iter}, i: {i}, j: {j}")
                             lt0 = time.perf_counter()
