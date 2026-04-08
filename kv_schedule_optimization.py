8f5b31c41ec22ce8c42bdcca5476e35501549643
Andrea Leang <42904081+AndreaLeang@users.noreply.github.com>
2026-04-07 17:20:52 -0700

    prints

--- kv_schedule_optimization.py
+++ kv_schedule_optimization.py
@@ -315,6 +315,8 @@ def recomp_calc_pred(opt_config, batch_size, prompt_len, cur_gen_len, recomp_len
     tot_energy = 0
     for each_ind in range(len(all_queries)):
         print(f"each_ind: {each_ind}")
+        print(f"all_queries: {all_queries[each_ind]}")
+        print(f"all_query_types: {all_query_types[each_ind]}")
         latency, _, energy = gpu_estimator.lookup(all_queries[each_ind], all_query_types[each_ind], target_freq=target_freq, lookup_target='all')
         tot_lat += latency
         tot_energy += energy
