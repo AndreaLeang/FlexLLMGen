7347d31691492586f92e19668383508985e4fd06
Andrea Leang <42904081+AndreaLeang@users.noreply.github.com>
2026-04-12 10:55:50 -0700

    reordered things

--- kv_schedule_optimization.py
+++ kv_schedule_optimization.py
@@ -138,34 +138,10 @@ def strategy_prediction(model, num_of_prompts, prompt_len, gen_len, hardware_con
     num_hidden_layers = model.num_hidden_layers
 
     # Forward Pass Prediction
-    #is_load_store: 0: none, 1: load only, 2: store only, 3: load and store
-  
-    
-    print(f"store_output_latency: {store_output_latency}")
-    print(f"no_store_output_latency: {no_store_output_latency}")
-    print(f"default_output_energy: {default_output_energy}")
+    # layer prediction: is_load_store: 0: none, 1: load only, 2: store only, 3: load and store
   
     for cur_gen_len in range(gen_len):
-        # input: nothing*(num_batches -1) + load
-        load_input_energy, load_input_latency = layer_prediction(model, 1, batch_size, num_batches, offload_percent, recomp_len, prompt_len, cur_gen_len, hardware_config, gpu_estimator, "input")
-        no_load_input_energy, no_load_input_latency = layer_prediction(model, 0, batch_size, num_batches, offload_percent, recomp_len, prompt_len, cur_gen_len, hardware_config, gpu_estimator, "input")
-        
-        input_energy = (num_batches-1)*no_load_input_energy + load_input_energy 
-        input_latency = (num_batches-1)*no_load_input_latency + load_input_latency 
-
-        # output: store + nothing*(num_batches -1) 
-        store_output_energy, store_output_latency =  layer_prediction(model, 2, batch_size, num_batches, offload_percent, recomp_len, prompt_len, cur_gen_len, hardware_config, gpu_estimator, "output")
-        no_store_output_energy, no_store_output_latency = layer_prediction(model, 0, batch_size, num_batches, offload_percent, recomp_len, prompt_len, cur_gen_len, hardware_config, gpu_estimator, "output")
-        default_output_energy = (num_batches-1)*no_store_output_energy
-        default_output_latency = (num_batches-1)*no_store_output_latency
-        if cur_gen_len == gen_len -1:
-            output_energy = default_output_energy + no_store_output_energy
-            output_latency = default_output_latency + no_store_output_latency
-        else: 
-            output_energy = default_output_energy + store_output_energy
-            output_latency = default_output_latency + store_output_latency
-
-        # MHA abd MLP
+        # input, output, MHA and MLP
         if num_batches == 1:
             #input
             # input: nothing*(num_batches -1) + load
