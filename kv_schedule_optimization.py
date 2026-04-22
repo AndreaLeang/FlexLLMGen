8f668c570149fce62be8b50e2a1075543e1bb8f2
Andrea Leang <42904081+AndreaLeang@users.noreply.github.com>
2026-04-22 12:28:10 -0700

    test add recomp pinned

--- kv_schedule_optimization.py
+++ kv_schedule_optimization.py
@@ -515,6 +515,7 @@ def layer_prediction(opt_config, is_load_store, batch_size, num_of_batches, offl
     else:
         recomp_bytes, kv_load_bytes = get_bytes_to_load(opt_config, batch_size, num_of_batches, offload_percent, recomp_len, prompt_len, gen_len)
         print(f"recomp_bytes: {recomp_bytes}, kv_load_bytes: {kv_load_bytes}")
+        recomp_pinned_energy, recomp_pinned_latency = pinned_pred(recomp_bytes, hardware_config, gpu_estimator)
         
         #recomp transfer & first kv load are always single directional. the second kv load uses single_directional indicator
         pinned_energy, pinned_latency = pinned_pred(kv_load_bytes, hardware_config, gpu_estimator)
@@ -531,9 +532,9 @@ def layer_prediction(opt_config, is_load_store, batch_size, num_of_batches, offl
             v_transfer_energy, v_transfer_latency = transfer_pred(kv_load_bytes, hardware_config, gpu_estimator, single_directional = False)
         # print(f"load and store second half: latency is max of pinned + layer calc: {pinned_latency  + layer_calc_latency} and transfer: {k_transfer_latency + v_transfer_latency}")
         second_half_latency = max(pinned_latency + layer_calc_latency, k_transfer_latency + v_transfer_latency)
-        tot_energy = pinned_energy + recomp_transfer_energy + layer_calc_energy + k_transfer_energy + v_transfer_energy
+        tot_energy = recomp_pinned_energy + pinned_energy + recomp_transfer_energy + layer_calc_energy + k_transfer_energy + v_transfer_energy
       
-        return tot_energy, first_half_latency + second_half_latency, recomp_transfer_energy + k_transfer_energy + v_transfer_energy, layer_calc_energy
+        return tot_energy, recomp_pinned_latency + first_half_latency + second_half_latency, recomp_transfer_energy + k_transfer_energy + v_transfer_energy, layer_calc_energy
 
 
 def pinned_pred(bytes, hardware_config, gpu_estimator):
