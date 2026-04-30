968c94dd0b5d8aac2aaa768dd4f3bba457cd5ee9
Andrea Leang <42904081+AndreaLeang@users.noreply.github.com>
2026-04-29 17:26:26 -0700

    where and softmax mha change

--- kv_schedule_optimization.py
+++ kv_schedule_optimization.py
@@ -739,8 +739,8 @@ def layer_calc_pred(opt_config, prompt_len, gen_len, batch_size, hardware_config
         bmm_query = {
         	'batch': batch_size*num_head,
         	'dimM' : prev_not_seen,
-        	'dimN' : head_dim,
-        	'dimK' : cur_seq_len,
+        	'dimN' : cur_seq_len,
+        	'dimK' : head_dim,
         	'precM': 'bf16',
         	'precA': 'bf16',
         	'useTensorCore':  True
@@ -751,7 +751,7 @@ def layer_calc_pred(opt_config, prompt_len, gen_len, batch_size, hardware_config
 
         # torch.where() → elementwise [batch_size, 1, 1, cur_seq_len] skim over [batch_size, num_head, 1, cur_seq_len]
         where_query = {
-            'dim': num_head,
+            'dim': batch_size*prev_not_seen*cur_seq_len,
             'op': 'unspecified_tensor',
             'prec': 'bf16',
         }
@@ -759,7 +759,7 @@ def layer_calc_pred(opt_config, prompt_len, gen_len, batch_size, hardware_config
         all_queries.append(where_query)
         all_query_types.append(where_query_type)
       
-        softmax_query = {'batch': batch_size*num_head,
+        softmax_query = {'batch': batch_size*num_head*prev_not_seen,
                          'dim': cur_seq_len, 
                          'prec': 'bf16'}
         softmax_query_type = ('softmax')
@@ -769,8 +769,8 @@ def layer_calc_pred(opt_config, prompt_len, gen_len, batch_size, hardware_config
         bmm_query = {
         	'batch': batch_size*num_head,
         	'dimM' : prev_not_seen,
-        	'dimN' : cur_seq_len,
-        	'dimK' : head_dim,
+        	'dimN' : head_dim,
+        	'dimK' : cur_seq_len,
         	'precM': 'bf16',
         	'precA': 'bf16',
         	'useTensorCore':  True
