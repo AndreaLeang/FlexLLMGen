24fdb196eeaf2522838cfe50535da8934ca29b29
Andrea Leang <42904081+AndreaLeang@users.noreply.github.com>
2026-05-18 12:09:33 -0700

    testing

--- kv_schedule_optimization.py
+++ kv_schedule_optimization.py
@@ -920,8 +920,8 @@ def disect_input(model, opt_config, num_of_prompts, prompt_len, gen_len, hardwar
         batch_sizes = [1, 2, 4, 8]
         all_feasible_strategies_dict = {1: [100], 2:[50], 4:[50], 8:[60]}
 
-        # batch_sizes = [1, 2, 4]
-        # all_feasible_strategies_dict = {1: [100], 2:[100], 4:[100]}
+        batch_sizes = [1, 2, 4]
+        all_feasible_strategies_dict = {1: [100], 2:[50], 4:[100]}
 
         # batch_sizes = [8]
         # all_feasible_strategies_dict = {8:[100]}
