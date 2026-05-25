c5248074abb94b182084c0b2283fbef84387eb31
Andrea Leang <42904081+AndreaLeang@users.noreply.github.com>
2026-05-25 08:18:48 -0700

    typo

--- kv_timer_verifier.py
+++ kv_timer_verifier.py
@@ -25,7 +25,7 @@ def get_all_gpu_memcpy_correlations(json_filename, get_cpu_time=False, est_bandw
         # load json file
         data = json.load(file)
 
-    gbs, prompt_len, gen_len, recomp_len = break_down_filename(filename)   
+    gbs, prompt_len, gen_len, recomp_len = break_down_filename(json_filename)   
     
     # initialize the dictionary to hold the ac2g
     ac2g_dict = {}
