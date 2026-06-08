93374cfa06c948230dd0032357185a717469f3f0
Andrea Leang <42904081+AndreaLeang@users.noreply.github.com>
2026-06-08 15:05:58 -0700

    prints

--- kv_schedule_optimization.py
+++ kv_schedule_optimization.py
@@ -171,6 +171,10 @@ def fast_strat_prediction(model, num_of_prompts, prompt_len, gen_len, hardware_c
     KV_transfer_7 = 0.0
 
     component_breakdown = other_token_component_breakdown / other_token_latency * 100
+    print(f"fast strat pred output: ")
+    print(f"tot_energy: {tot_energy}, tot_latency: {tot_latency}, time_to_first_token: {time_to_first_token}, avg_energy_per_layer: {avg_energy_per_layer}, avg_latency_per_layer: {avg_latency_per_layer}")
+    print(f"percent_energy_offloading: {percent_energy_offloading}, percent_energy_active: {percent_energy_active}, percent_latency_transfer: {percent_latency_transfer}")
+    print(f"component_breakdown: {component_breakdown}")
   
     # get total energy and latency and time_to_first_token
     return tot_energy, tot_latency, time_to_first_token, avg_energy_per_layer, avg_latency_per_layer, percent_energy_offloading, percent_energy_active, percent_latency_transfer, component_breakdown
