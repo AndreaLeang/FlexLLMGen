c8679467fdb7841edd5a59ab506f632ebfbe3ff1
Kyungmi Lee <kyungmi.lee@rice.edu>
2026-08-04 02:54:06 +0000

    sync

--- scripts-kyungmi/setup.sh
+++ scripts-kyungmi/setup.sh
@@ -3,4 +3,5 @@ source /root/miniconda3/bin/activate
 export CONDA_PLUGINS_AUTO_ACCEPT_TOS=yes
 conda env create -f conda_env.yml
 conda activate flexgen_env
+pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu128
 pip install -e .
--- scripts-kyungmi/setup_blackwell.sh
+++ scripts-kyungmi/setup_blackwell.sh
@@ -1,5 +1,7 @@
 bash install_conda.sh
 source /root/miniconda3/bin/activate
+export CONDA_PLUGINS_AUTO_ACCEPT_TOS=yes
 conda env create -f conda_env_blackwell.yml
 conda activate flexgen_env_blackwell
+pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu128
 pip install -e .
